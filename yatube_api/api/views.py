d543c39401e6d2edfcedc1b9377d4c9cb914b0db
Yuliy <yuliypavlov@gmail.com>
2023-10-27 15:30:18 +0500

    Updated project

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -46,7 +46,7 @@ class FollowViewSet(mixins.ListModelMixin, mixins.CreateModelMixin,
     serializer_class = FollowSerializer
     permission_classes = IsAuthenticated,
     filter_backends = filters.SearchFilter,
-    search_fields = ('following__username',)
+    search_fields = 'following__username',
 
     def get_queryset(self):
         return self.request.user.follower.all()
--- yatube_api/yatube_api/urls.py
+++ yatube_api/yatube_api/urls.py
@@ -2,14 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from api.views import FollowViewSet
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('api.urls')),
     path('api/v1/', include('djoser.urls.jwt')),
-    path('api/v1/follow/', FollowViewSet),
     path(
         'redoc/',
         TemplateView.as_view(template_name='redoc.html'),
